6f93268316939587b6d68f429d3d64d161edf5c4
DanielPomareda <daniel.pomareda@innite.net>
2024-03-18 23:21:41 -0500

    subiendo responsive de navbar y quienes somos
    subiendo responsive de navbar y quienes somos

---
+++ jquery.js
@@ -0,0 +1,11 @@
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarTogglerBtn = document.getElementById("navbar-toggler-btn");
+    const navbarCollapse = document.getElementById("navbar-collapse");
+
+    navbarTogglerBtn.addEventListener("click", function() {
+        navbarCollapse.classList.toggle("show");
+    });
+});
+
+*/
